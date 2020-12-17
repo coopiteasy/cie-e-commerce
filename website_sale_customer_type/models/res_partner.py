a7418a6e3c45a5e6a9d59890f634ab956943dec8
Rémy Taymans <remytms@tsmail.eu>
2020-12-17 17:27:39 +0100

    [ADD] w_s_customer_type

---
+++ website_sale_customer_type/models/res_partner.py
@@ -0,0 +1,54 @@
+# Copyright 2020 Coop IT Easy SCRLfs <http://coopiteasy.be>
+#   Rémy Taymans <remy@coopiteasy.be>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import _, api, exceptions, fields, models
+
+
+class ResPartner(models.Model):
+    _name = "res.partner"
+    _inherit = "res.partner"
+
+    customer_type_id = fields.Many2one(
+        comodel_name="res.partner.customer.type",
+        string="Customer Type",
+        help=(
+            "Customer type used to distinguish different type of customer"
+        )
+    )
+    website_restrict_product = fields.Boolean(
+        string="Restrict Product on E-commerce",
+        compute="_compute_website_restrict_product"
+    )
+    website_product_ids = fields.Many2many(
+        comodel_name="product.product",
+        string="Product",
+        compute="_compute_website_product_ids",
+    )
+
+    @api.depends("customer_type_id")
+    def _compute_website_restrict_product(self):
+        """
+        Set website_restrict_product to True if the customer type
+        has the flag website_restrict_product set.
+        """
+        for partner in self:
+            partner.website_restrict_product = (
+                partner.customer_type_id
+                and partner.customer_type_id.website_restrict_product
+            )
+
+    @api.depends("customer_type_id")
+    def _compute_website_product_ids(self):
+        """
+        Return the list of product that can be seen on the e-commerce by
+        the partner if connected depending on its customer type.
+        """
+        for partner in self:
+            products = self.env["product.product"]
+            if (
+                partner.customer_type_id
+                and partner.customer_type_id.website_product_ids
+            ):
+                products |= partner.customer_type_id.website_product_ids
+            partner.website_product_ids = products
