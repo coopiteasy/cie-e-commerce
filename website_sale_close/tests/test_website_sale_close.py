ed9b59458559506979a84157118dbeaf2e440bed
Rémy Taymans <remytms@tsmail.eu>
2021-01-21 18:12:25 +0100

    [ADD] w_s_close: tests

---
+++ website_sale_close/tests/test_website_sale_close.py
@@ -0,0 +1,27 @@
+# Copyright 2019-Today Coop IT Easy SCRLfs
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.tests.common import HttpCase
+
+
+class WebsiteSaleCloseCase(HttpCase):
+
+    def test_01_check_e_commerce_open(self):
+        website = self.env["website"].browse(1)
+        website.is_ecommerce_open = True
+        result = self.url_open("/shop")
+        self.assertNotIn(website.text_closed_ecommerce, result.text)
+        result = self.url_open("/shop/cart")
+        self.assertNotIn(website.text_closed_ecommerce, result.text)
+        result = self.url_open("/shop/checkout")
+        self.assertNotIn(website.text_closed_ecommerce, result.text)
+
+    def test_02_check_e_commerce_close(self):
+        website = self.env["website"].browse(1)
+        website.is_ecommerce_open = False
+        result = self.url_open("/shop")
+        self.assertIn(website.text_closed_ecommerce, result.text)
+        result = self.url_open("/shop/cart")
+        self.assertIn(website.text_closed_ecommerce, result.text)
+        result = self.url_open("/shop/checkout")
+        self.assertIn(website.text_closed_ecommerce, result.text)
