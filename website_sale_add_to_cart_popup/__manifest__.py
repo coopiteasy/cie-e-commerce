9aa490f46c4faf1c959f23c36ebd758a356858d1
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-03-03 11:06:52 +0100

    [14.0][MIG] website_sale_add_to_cart_popup: Migration to 14.0
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- website_sale_add_to_cart_popup/__manifest__.py
+++ website_sale_add_to_cart_popup/__manifest__.py
@@ -4,9 +4,12 @@
 {
     "name": "Website Sale Add To Cart Popup",
     "summary": "Always show the add to cart popup in the e-commerce.",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Coop IT Easy SCRLfs",
     "website": "https://coopiteasy.be",
-    "depends": ["website_sale"],
+    "depends": [
+        "sale_product_configurator",
+        "website_sale",  # Recommends
+    ],
 }
--- website_sale_add_to_cart_popup/controllers/product_configurator.py
+++ website_sale_add_to_cart_popup/controllers/product_configurator.py
@@ -1,10 +1,9 @@
 # Copyright 2021 Coop IT Easy SCRLfs <https://coopiteasy.be>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields
 from odoo.http import request
 
-from odoo.addons.sale.controllers.product_configurator import (
+from odoo.addons.sale_product_configurator.controllers.main import (
     ProductConfiguratorController,
 )
 
@@ -16,55 +15,42 @@ class ProductConfiguratorController(ProductConfiguratorController):
         """
         Overwrite default behaviour to always show a content for the
         add to cart modal.
+
+        Copied verbatim from sale_product_configurator, and slightly adjusted.
         """
-        product = (
-            request.env["product.product"]
-            .with_context(self._get_product_context(pricelist, **kw))
-            .browse(int(product_id))
-        )
+        product = request.env["product.product"].browse(int(product_id))
         combination = request.env["product.template.attribute.value"].browse(
             variant_values
         )
+        # Commented out from upstream. Preserved for posterity's sake.
+        #
+        # has_optional_products = product.optional_product_ids.filtered(
+        #     lambda p: p._is_add_to_cart_possible(combination)
+        # )
+        #
+        # if not has_optional_products:
+        #     return False
 
         add_qty = int(kw.get("add_qty", 1))
-        to_currency = (pricelist or product).currency_id
-        company = (
-            request.env["res.company"].browse(
-                request.env.context.get("company_id")
-            )
-            or request.env["res.users"]._get_company()
-        )
-        date = request.env.context.get("date") or fields.Date.today()
-
-        def compute_currency(price):
-            return product.currency_id._convert(
-                price, to_currency, company, date
-            )
 
         no_variant_attribute_values = combination.filtered(
-            lambda rec: rec.attribute_id.create_variant == "no_variant"
+            lambda product_template_attribute_value: product_template_attribute_value.attribute_id.create_variant  # noqa: B950
+            == "no_variant"
         )
         if no_variant_attribute_values:
             product = product.with_context(
                 no_variant_attribute_values=no_variant_attribute_values
             )
 
-        return request.env["ir.ui.view"].render_template(
-            "sale.optional_products_modal",
+        return request.env["ir.ui.view"]._render_template(
+            "sale_product_configurator.optional_products_modal",
             {
                 "product": product,
                 "combination": combination,
                 "add_qty": add_qty,
-                # reference_product deprecated, use combination instead
-                "reference_product": product,
+                "parent_name": product.name,
                 "variant_values": variant_values,
                 "pricelist": pricelist,
-                # compute_currency deprecated, get from pricelist or product
-                "compute_currency": compute_currency,
-                # to_currency deprecated, get from pricelist or product
-                "to_currency": to_currency,
                 "handle_stock": handle_stock,
-                # get_attribute_exclusions deprecated, use product method
-                "get_attribute_exclusions": self._get_attribute_exclusions,
             },
         )
