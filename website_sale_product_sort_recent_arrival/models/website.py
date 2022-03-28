d6df14854f1cea0cc6a38ec33aae534133879962
Rémy Taymans <remytms@tsmail.eu>
2022-03-28 19:44:29 +0200

    [ADD] website_sale_product_sort_recent_arrival

---
+++ website_sale_product_sort_recent_arrival/models/website.py
@@ -0,0 +1,20 @@
+# Copyright 2022 Coop IT Easy SCRLfs
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import _, api, models
+
+
+class Website(models.Model):
+
+    _inherit = "website"
+
+    @api.model
+    def _get_product_sort_criterias(self):
+        res = super()._get_product_sort_criterias()
+        res.append(
+            (
+                "recent_arrival_date desc",
+                _("Recent Arrival Date (Recent first)"),
+            )
+        )
+        return res
