64281611b46fb468dafef96a3446aee3329662bb
Rémy Taymans <remytms@tsmail.eu>
2022-03-30 16:36:49 +0200

    [ADD] website_sale_delivery_product_restriction

---
+++ website_sale_delivery_product_restriction/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 Coop IT Easy SCRLfs
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Website Sale Delivery Product Restriction",
+    "summary": """
+        Allow some product to be shipped only by some delivery carrier and also on eCommerce.""",
+    "version": "12.0.1.0.0",
+    "category": "Website",
+    "website": "https://coopiteasy.be",
+    "author": "Coop IT Easy SCRLfs",
+    "license": "AGPL-3",
+    "application": False,
+    "depends": ["delivery_product_restriction"],
+    "excludes": [],
+    "data": [],
+    "demo": [],
+    "qweb": [],
+}
