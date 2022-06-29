d60bc3491fcc6538ef6a81fb23da642e0606a698
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-06-29 11:33:26 +0200

    [FIX] SCRLfs → SC
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

--- website_sale_add_to_cart_popup/controllers/product_configurator.py
+++ website_sale_add_to_cart_popup/controllers/product_configurator.py
@@ -1,4 +1,4 @@
-# Copyright 2021 Coop IT Easy SCRLfs <https://coopiteasy.be>
+# Copyright 2021 Coop IT Easy SC <https://coopiteasy.be>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields
