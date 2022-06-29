166a01eee92627547f0a42a2b7b459631f14dd4b
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-06-29 11:33:26 +0200

    [FIX] SCRLfs → SC
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- website_sale_stock_auto_publish/README.rst
+++ website_sale_stock_auto_publish/README.rst
@@ -42,7 +42,7 @@ Credits
 Authors
 ~~~~~~~
 
-* Coop IT Easy SCRLfs <https://coopiteasy.be>
+* Coop IT Easy SC <https://coopiteasy.be>
 
 Contributors
 ~~~~~~~~~~~~
--- website_sale_stock_auto_publish/__init__.py
+++ website_sale_stock_auto_publish/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2021 Coop IT Easy SCRLfs <http://coopiteasy.be>
+# Copyright 2021 Coop IT Easy SC <http://coopiteasy.be>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import models
