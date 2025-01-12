14a0b191a74c59293ed7b56728f92e19a1346c47
hugues de keyzer <odoo@hugues.info>
2025-01-12 12:04:17 +0100

    [MIG] website_sale_sort_variants_by_availability: migration to 16.0

--- website_sale_sort_variants_by_availability/models/product_template.py
+++ website_sale_sort_variants_by_availability/models/product_template.py
@@ -10,9 +10,10 @@ class ProductTemplate(models.Model):
 
     def _get_possible_variants_sorted(self, parent_combination=None):
         # this is used by the website_sale.product_variants, which is not used
-        # by default, but can be enabled through website > customize > list
-        # view of variants. this only affects product templates that use only
-        # one variant attribute; the other ones still use the default
+        # by default, but can be enabled by navigating to a product page on
+        # the website, clicking edit, and in the customize tab, select
+        # variants: products list. this only affects product templates that
+        # use only one variant attribute; the other ones still use the default
         # template.
         self.ensure_one()
         return (
