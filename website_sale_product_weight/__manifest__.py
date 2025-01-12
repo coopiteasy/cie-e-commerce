851f15cbcec99798e3b73efd4d53fac7cf2523b0
hugues de keyzer <odoo@hugues.info>
2025-01-12 12:41:17 +0100

    [MIG] website_sale_product_weight: migration to 14.0

--- website_sale_product_weight/__manifest__.py
+++ website_sale_product_weight/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Website Sale Product Weight",
     "summary": "Display the weight of a product on the e-commerce product page",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Website",
     "website": "https://coopiteasy.be",
     "author": "Coop IT Easy SC",
